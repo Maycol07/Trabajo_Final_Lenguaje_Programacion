9907acbf928dd272b342d307f05fe6709f80992c
Mrx18 <115897516+MaycolRichardClaudioPinedo18@users.noreply.github.com>
2022-10-24 11:18:03 -0500

    Conexio a SQL

---
+++ Conexion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD
+{
+    public class Conexion
+    {
+        private static readonly Conexion instancia = new Conexion();
+
+        public static Conexion Instancia
+        {
+            get { return instancia; }
+        }
+
+        public SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source = DESKTOP-FFNB1QU; Initial Catalog = Estudiante; Integrated Security = true";
+            
+            return cn;
+        }
+    }
+}
